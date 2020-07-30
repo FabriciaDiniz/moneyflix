daddca2684c27c23ccb551062766bb1e3152a2be
Fabrícia Diniz <afabriciadiniz@gmail.com>
2020-07-29 22:20:12 -0300

    conteúdos das aulas 1 e 2

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import './Menu.css';
+import Button from '../Button';
+
+function Menu() {
+	return (
+		<nav className="Menu">
+			<Link to="/">
+				<img className="Logo" src={logo} alt="logo fabsflix"></img>
+			</Link>
+
+			<Button as={Link} className="ButtonLink" to="/cadastro/video">
+				Novo vídeo
+			</Button>
+		</nav>
+	);
+}
+
+export default Menu;
