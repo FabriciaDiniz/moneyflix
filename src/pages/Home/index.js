dcf33887daadb7b5b74a691c130bb22d1759e3fa
Fabrícia Diniz <afabriciadiniz@gmail.com>
2021-07-14 21:54:35 -0300

    update video description

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ function Home() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Nesse vídeo a Nath desvenda alguns termos técnicos que a gente escuta por aí e parecem um bicho de 7 cabeças, mas não são."}
       />
 
       <Carousel
